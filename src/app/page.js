130f35634426d87d5c4435e98758ec0c8a68a2e5
marufsiyam91 <marufsiyam9123@gmail.com>
2024-04-24 17:55:22 +0600

    main page decorated with all element

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,29 @@
+import BookBar from "./Components/BookBar";
+import SideNav from "./Components/SideNav";
+import TopNavigateBar from "./Components/TopNavigateBar";
+import HadithCard from './Components/HadithCard';
+import Settingsbar from "./Components/Settingsbar";
 
 export default function Home() {
   return (
-    <main className="h-[100vh] w-full bg-white">
-
+    <main className="h-[100vh] w-full bg-white overflow-hidden">
+      <TopNavigateBar/>
+      <div className="flex justify-between h-auto">
+        <div >
+          <SideNav/>
+        </div>
+        <div className="w-full rounded-tl-[30px] bg-[#F2F4F6] flex gap-4 p-4">
+          <div className="">
+            <BookBar/>
+          </div>
+          <div className="w-full">
+            <HadithCard/>
+          </div>
+          <div className="4xl:block hidden">
+            <Settingsbar/>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
